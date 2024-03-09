4bdacf1df6a9bdd78ae537d8ca5b3767070d3a9a
dmeshkovGit <140300183+dmeshkovGit@users.noreply.github.com>
2024-03-09 11:49:20 +0200

    create routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryList, Grid, Heading, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/countryApi';
 
 export const Home = () => {
+const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+    try {
+      const data = await getCountries()
+      setCountries(data)
+    } catch (error) {
+      
+    }
+    }
+    getData();
+  },[])
+
   return (
     <Section>
       <Container>
-        <Heading title="Home" bottom />
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
